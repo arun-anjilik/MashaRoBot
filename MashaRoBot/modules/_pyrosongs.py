fbd9e5963ffaf502e80072885f46b7222556f8e7
arun-anjilik <52011992+arun-anjilik@users.noreply.github.com>
2022-07-19 14:38:56 +0530

    Update _pyrosongs.py

--- MashaRoBot/modules/_pyrosongs.py
+++ MashaRoBot/modules/_pyrosongs.py
@@ -74,7 +74,7 @@ def song(client, message):
     except Exception as e:
         print(e)
 
-@pbot.on_message(filters.text(['play']))
+@pbot.on_message(filters.regex("play"))
 def song(client, message):
 
     user_id = message.from_user.id 
